0819eb57489eb582cd12c7bcfb4d8e7a37f838d3
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-09-21 00:29:05 +0300

    add storage

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import reportWebVitals from './reportWebVitals';
 import i18n from "./i18n";
 import { I18nextProvider } from "react-i18next";
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
+      <Provider store={store}>
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
